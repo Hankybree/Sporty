99c50d142d638e9ddfe8823b58c464a05ae4c628
Henrik Frank <frank.havsvinge@gmail.com>
2020-05-27 09:42:47 +0200

    Created file structure for frontend and initialized VueRouter and Vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import App from './App.vue'
+
+import HomeView from './views/HomeView.vue'
+import ProfileView from './views/ProfileView.vue'
+import EventView from './views/EventView.vue'
+
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+const router = new VueRouter({
+  routes: [
+    { component: HomeView, path: '/'},
+    { component: ProfileView, path: '/profile/:user' },
+    { component: EventView, path: '/events' }
+  ]
+})
+
+const store = new Vuex.Store({
+  state: {
+
+  }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
---
+++ frontend/src/views/EventView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Eventasd
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'EventView'
+}
+</script>
+
+<style scoped>
+
+</style>
