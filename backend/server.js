ee626b9dc0d69e0b576c3332348cd2d4927a4cb9
jakob salomonsson <jakob@salomonssons.se>
2020-05-27 11:43:58 +0200

    implementerat post och testat att det fungerar

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const cors = require('cors')
+const sqlite = require('sqlite')
+const sqlite3 = require('sqlite3')
 
 const app = express()
 
-app.listen({ port: 3000 }, () => {
+app.use(express.json())
+app.use(cors())
+
+app.listen({ port: 12002 }, () => {
     console.log('nu funkar det')
 })
+
+let database
+
+sqlite
+    .open({ driver: sqlite3.Database, filename: 'database.sqlite' })
+    .then((database_) => {
+        database = database_
+    })
 
 app.get('/', (request, response) => {
     response.send('get one')
@@ -14,8 +28,23 @@ app.get('/', (request, response) => {
     response.send('get many')
 })
 
-app.post('/', (request, response) => {
-    response.send('post')
+app.post('/events', (request, response) => {
+    database
+        .run('INSERT INTO events (eventSport, eventTitle, eventDescription, eventGoers, eventUser) VALUES (?, ?, ?, ?, ?)',
+            [
+                request.body.eventSport,
+                request.body.eventTitle,
+                request.body.eventDescription,
+                request.body.eventGoers,
+                request.body.eventUser
+            ])
+
+        .then(() => {
+            response.status(200).send('Event Created!')
+        })
+        .catch(error => {
+            response.send(error)
+        })
 })
 
 app.delete('/', (request, response) => {
